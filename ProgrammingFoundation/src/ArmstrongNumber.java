6e14be8733b0574610fe7a212dcdb10925668073
Administrator <Administrator@172.31.16.234>
2023-04-26 07:47:19 +0000

    armstrong class update

--- ProgrammingFoundation/src/ArmstrongNumber.java
+++ ProgrammingFoundation/src/ArmstrongNumber.java
@@ -1,27 +1,26 @@
 
-public class ArmstringNumber {
+public class ArmstrongNumber {
 //153: {1(3)+3(3)+5(3)
-	
+
 	public static void main(String[] args) {
-		boolean result=isArmstrong(153);
+		boolean result = isArmstrong(153);
 		System.out.println(result);
 	}
-	public static boolean isArmstrong(int number) {
-	int digit ;
-	int temp=number;
-	int sum=0;
-	while (number>0) {
-		digit=number%10;
-		sum=sum+ digit*digit*digit;
-		number=number/10;
-					
-	}
-	if (temp==sum) {
-		return true;
-		}
-	return false;
-	}
-	
-	}
-	
 
+	public static boolean isArmstrong(int number) {
+		int digit;
+		int temp = number;
+		int sum = 0;
+		while (number > 0) {
+			digit = number % 10;
+			sum = sum + digit * digit * digit;
+			number = number / 10;
+
+		}
+		if (temp == sum) {
+			return true;
+		}
+		return false;
+	}
+
+}
